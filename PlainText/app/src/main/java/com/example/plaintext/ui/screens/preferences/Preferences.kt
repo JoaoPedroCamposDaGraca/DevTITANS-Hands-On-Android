f203dbaf0b0aef064e4c918d3c183b2d66e90b99
pedrobelota <henriquebgpedro@gmail.com>
2026-06-12 11:47:07 -0400

    feat: Task 3.2/3.3

--- PlainText/app/src/main/java/com/example/plaintext/ui/screens/preferences/Preferences.kt
+++ PlainText/app/src/main/java/com/example/plaintext/ui/screens/preferences/Preferences.kt
@@ -1,81 +1,82 @@
 package com.example.plaintext.ui.screens.preferences
 
-
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
 import com.example.plaintext.ui.screens.login.TopBarComponent
 import com.example.plaintext.ui.screens.util.PreferenceInput
 import com.example.plaintext.ui.screens.util.PreferenceItem
 import com.example.plaintext.ui.viewmodel.PreferencesState
 import com.example.plaintext.ui.viewmodel.PreferencesViewModel
-import kotlinx.coroutines.delay
 
 @Composable
-fun SettingsScreen(navController: NavHostController?,
-                   viewModel: PreferencesViewModel = hiltViewModel()
-){
+fun SettingsScreen(
+    viewModel: PreferencesViewModel = hiltViewModel()
+) {
     Scaffold(
         topBar = {
             TopBarComponent()
         }
-    ){ padding ->
-        SettingsContent(modifier = Modifier.padding(padding), viewModel)
+    ) { padding ->
+        SettingsContent(
+            modifier = Modifier.padding(padding),
+            preferencesState = viewModel.preferencesState,
+            updateLogin = viewModel::updateLogin,
+            updatePassword = viewModel::updatePassword,
+            updatePreencher = viewModel::updatePreencher
+        )
     }
 }
 
 @Composable
-fun SettingsContent(modifier: Modifier = Modifier, viewModel: PreferencesViewModel) {
+fun SettingsContent(
+    modifier: Modifier = Modifier,
+    preferencesState: PreferencesState,
+    updateLogin: (String) -> Unit,
+    updatePassword: (String) -> Unit,
+    updatePreencher: (Boolean) -> Unit
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(16.dp)
-            .verticalScroll(rememberScrollState())){
-
+            .verticalScroll(rememberScrollState())
+    ) {
         PreferenceInput(
             title = "Preencher Login",
             label = "Login",
-            fieldValue = "",
-            summary = "Preencher login na tela inicial"
-        ){
-            // função para alterar o login
-        }
+            fieldValue = preferencesState.login,
+            summary = "Preencher login na tela inicial",
+            onFinish = updateLogin
+        )
 
         PreferenceInput(
             title = "Setar Senha",
-            label = "Label",
-            fieldValue = "",
-            summary = "Senha para entrar no sistema"
-        ){
-            // função para alterar a senha
-        }
+            label = "Senha",
+            fieldValue = preferencesState.password,
+            summary = "Senha para entrar no sistema",
+            onFinish = updatePassword
+        )
 
         PreferenceItem(
             title = "Preencher Login",
             summary = "Preencher login na tela inicial",
             onClick = {
-                // deve alterar o estado que representa se o switch está ligado ou não
+                updatePreencher(!preferencesState.preencher)
             },
             control = {
                 Switch(
-                    checked = false, // deve ler o estado que representa se o switch está ligado ou não
-                    onCheckedChange = {
-                        // deve alterar o estado que representa se o switch está ligado ou não
-                    }
+                    checked = preferencesState.preencher,
+                    onCheckedChange = updatePreencher
                 )
             }
         )
@@ -85,5 +86,10 @@ fun SettingsContent(modifier: Modifier = Modifier, viewModel: PreferencesViewMod
 @Preview(showBackground = true)
 @Composable
 fun SettingsScreenPreview() {
-    SettingsScreen(null)
+    SettingsContent(
+        preferencesState = PreferencesState(),
+        updateLogin = {},
+        updatePassword = {},
+        updatePreencher = {}
+    )
 }
